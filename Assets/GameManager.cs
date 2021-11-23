c4abdc55d1505a8bc1986fc6fcf32fd490365fa0
songheeseon <gmltjs6128@naver.com>
2021-11-23 13:04:29 +0900

    똥피하기 게임 완성

---
+++ Assets/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+            _instance = FindObjectOfType<GameManager>();
+            }
+        return _instance;
+        }
+    }
+
+
+    private int score;
+
+    [SerializeField]
+    private Text scoreTxT;
+
+
+    [SerializeField]
+    
+    private GameObject poop;
+    [SerializeField]
+    private Text BestScore;
+   
+    [SerializeField]
+    private GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public bool stopTrigger = true;
+
+    public void GameOver()
+    {
+        stopTrigger = false;
+
+        if (score >= PlayerPrefs.GetInt("BestScore", 0))
+            PlayerPrefs.SetInt("BestScore", score);
+
+        BestScore.text = PlayerPrefs.GetInt("BestScore", 0).ToString();
+
+        panel.SetActive(true);
+    }
+
+    public void GameStart()
+    {
+        scoreTxT.text = "Score : 0"; // 게임 끝나고 재시작할 때 점수는 0으로 표시 
+        score = 0; // 점수 값을 0으로 대입 
+        stopTrigger = true;
+        StartCoroutine(CreatepoopRoutine());
+        panel.SetActive(false);
+    }
+
+    public void Score()
+    {
+        if(stopTrigger)
+        score++;
+
+        Debug.Log("score: " + score);
+        scoreTxT.text = "Score : " +score;
+
+        poop.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+
+        if (score >= 30)
+        {
+            poop.GetComponent<Rigidbody2D>().gravityScale = 1.3f;
+        }else if(score >= 50)
+        {
+            poop.GetComponent<Rigidbody2D>().gravityScale = 1.5f;
+        }else if (score >= 100)
+        {
+            poop.GetComponent<Rigidbody2D>().gravityScale = 1.7f;
+        }
+    }
+
+    IEnumerator CreatepoopRoutine()
+    {
+        while (stopTrigger)
+        {
+            CreatePoop();
+            yield return  new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void CreatePoop()
+    {
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.05f, 0.95f), 1.1f, 0));
+        pos.z = 0.0f;
+        Instantiate(poop, pos, Quaternion.identity);
+    }
+}
+
---
+++ Assets/poop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poop : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            GameManager.instance.Score();
+            animator.SetTrigger("poop");
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(gameObject, 0.3f);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            GameManager.instance.GameOver();
+            animator.SetTrigger("poop");
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(gameObject, 0.3f);
+        }
+
+
+    }
+}
